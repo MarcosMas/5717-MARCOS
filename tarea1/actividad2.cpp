954fdc65337645d0f4718cf7ed5465799823c1b8
MarcosMas <antonio1997a.mamc@gmail.com>
2020-02-19 20:10:47 -0600

    actividad4

--- tarea1/actividad2.cpp
+++
@@ -1,6 +0,0 @@
-#include<iostream>
-using namespace std;
-int main()
-{
-unsigned int x;
-} 
--- tarea1/actividad4.cpp
+++ tarea1/actividad4.cpp
@@ -1,7 +1,32 @@
 #include<iostream>
 using namespace std;
+
 int main()
 {
-unsigned int num, pos;
-} 
+  int mayor = 0;
+  int menor = 0;
+  int valor = 0;
+  int esprimero = 1;
+  do
+  {
+    cin>>valor;   
+    if (cin&&valor<0) continue;
+    if(cin&& esprimero)
+    {
+      mayor = menor = valor;
+      esprimero= 0;
+    } 
+    else{
+       if (cin && valor>mayor)
+        mayor= valor;
+       if (cin && valor<mayor)
+        menor= valor;
+     }
+  }
+     while (cin);
+ 
+ cout<<"el numero mayor es: " <<mayor<<endl;
+ cout<<"el numero menor es: " <<menor<<endl;
+return 0;
+}
 
