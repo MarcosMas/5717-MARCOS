8198492235125b3ba8b2e3bebf44cc2271c4994c
MarcosMas <antonio1997a.mamc@gmail.com>
2020-02-26 22:01:09 -0600

    actividad14

--- tarea1/actividad14.cpp
+++ tarea1/actividad14.cpp
@@ -3,16 +3,3 @@
 using namespace std;
 int main()
 {
-	 int i, p, n, s=0;
-         cin>>n;
-     while(cin){
-         if(n%2==0)
-     p+=+1;
-     if(n%2==0&&p<=10)
-         s=s+n;
-             cin>>n;
-     }
-     
-	 cout<<"suma de los 10 pares:  "<<s<<endl;
-return 0;
-}
