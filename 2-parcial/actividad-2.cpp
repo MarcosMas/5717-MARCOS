a16b8e1fb8fe279b636325710fc761edb4502fed
MarcosMas <antonio1997a.mamc@gmail.com>
2020-06-15 14:57:41 -0500

    semana1

--- 2-parcial/actividad-2.cpp
+++ 2-parcial/actividad-2.cpp
@@ -1,49 +1,65 @@
-#include <iostream>
-#include <string>
+#include<iostream>
+#include<cstdlib>
+#include<ctime>
+#include<string>
 
 using namespace std;
 
-
-int valor1(long* a1,long* a2,long n,int a4,long k){
-             int x=0;
-             for(int i=0;i<a4;i++)
-             {
-                 for(long j=0;j<n;j++)
-                 {
-                     if((a1[j]<=(a2[i]+k))&&(a1[j]>=(a2[i]-k)))
-                     {
-                     x+=1;a2[i]=0;
-                     } 
-                 }
-             }
-      return x;
-      }
-long* valor2(long n){
-     long* a1=new long[n];
-         for(int i=0;i<n;i++)
-         cin>>a1[i];
-return a1;
+int fac(int n){
+int i, a=1;
+      if(n<0)
+                a=0;
+            else if(n==0)
+                a=1;
+                else
+{
+        for(i=1;i<=n;i++)
+        a=a*i;
 }
-long* valor3(int m){
-     long* a2=new long[m];
-         for(int i=0;i<m;i++)
-         cin>>a2[i];
-return a2;
+return a;
 }
 
-int main(){
+int main()
+{
+srand(time(NULL));
+int n, s, suma=0, e1, e2, e3, e4, e5;
+      cin>>n;
+      cin>>suma;
+                  int num[n];
+                     for(int i=0;i<n;i++)
+                         cin>>num[i];
+e1=fac(n);
+e2=fac(4);
+e3=n-4;
+e4=fac(e3);
+e5=e1/(e2*e4)+4;
 
-     int m;
-     long n,k;
-         cin>>n>>m>>k;
-         long* a1=new long[n];
-         long* a2=new long[m];
-                 if(n>=1&&m<=200000&&(0<=k<=1000000000))
-                 {
-                     a1=valor2(n);
-                     a2=valor3(m);
-                     int x=valor1(a1,a2,n,m,k);
-                     cout<<x<<endl;
-                 }
-     return 0; 
+int size=4;
+int ind;
+
+            for(int i=0;i<=e1;i++)
+
+{
+            for(int i=0;i<n;i++)
+                cout<<num[i]<<" ";
+ while(size>0)
+ 
+{
+ind=rand()%size;
+s=s+num[ind];
+int aux;
+        while(ind<size)
+        {
+        num[ind]=num[ind+1];
+        num[ind+1]=0;
+        ind++;
+                }
+ size--;
+           for(int i=0;i<=n;i++)
+        cout<<num[i]<<" ";
+                          }
+        cout<<"LA SUMA ES: "<<suma<<endl;
+}
+cout<<endl;
+return 0;
 }
