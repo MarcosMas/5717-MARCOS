46f99305241b5516594da811eda48b40b6830098
MarcosMas <antonio1997a.mamc@gmail.com>
2020-06-25 19:48:48 -0500

    semana-1

--- 2-parcial/actividad-2.cpp
+++ 2-parcial/actividad-2.cpp
@@ -1,65 +1,59 @@
-#include<iostream>
-#include<cstdlib>
-#include<ctime>
-#include<string>
-
+#include <iostream>
 using namespace std;
 
-int fac(int n){
-int i, a=1;
-      if(n<0)
-                a=0;
-            else if(n==0)
-                a=1;
-                else
-{
-        for(i=1;i<=n;i++)
-        a=a*i;
-}
-return a;
+void ver(int* cadena,int* ar_xP){
+if(ar_xP[0]==ar_xP[1]&&ar_xP[0]==ar_xP[2]&&ar_xP[0]==ar_xP[3]){
+  cout<<"IMxPIBLE"<<endl;}
+  else{
+    cout<<ar_xP[0]<<" "<<ar_xP[1]<<" "<<ar_xP[2]<<" "<<ar_xP[3]<<endl;
+  }
 }
 
-int main()
-{
-srand(time(NULL));
-int n, s, suma=0, e1, e2, e3, e4, e5;
-      cin>>n;
-      cin>>suma;
-                  int num[n];
-                     for(int i=0;i<n;i++)
-                         cin>>num[i];
-e1=fac(n);
-e2=fac(4);
-e3=n-4;
-e4=fac(e3);
-e5=e1/(e2*e4)+4;
-
-int size=4;
-int ind;
-
-            for(int i=0;i<=e1;i++)
-
-{
-            for(int i=0;i<n;i++)
-                cout<<num[i]<<" ";
- while(size>0)
- 
-{
-ind=rand()%size;
-s=s+num[ind];
-int aux;
-        while(ind<size)
-        {
-        num[ind]=num[ind+1];
-        num[ind+1]=0;
-        ind++;
-                }
- size--;
-           for(int i=0;i<=n;i++)
-        cout<<num[i]<<" ";
-                          }
-        cout<<"LA SUMA ES: "<<suma<<endl;
+int* generar(int n){
+int* cadena=new int[n];
+for(int k=0;k<n;k++){
+  cin>>cadena[k];
+  }
+return cadena;
 }
-cout<<endl;
+
+int verificar(int* cadena,int* xP,int x){
+if(cadena[xP[0]]+cadena[xP[1]]+cadena[xP[2]]+cadena[xP[3]]==x){
+  return 1;
+  }
+else{ 
+return 0;
+  }
+}
+
+
+int* calcular(int* cadena,int n,int x,int y){
+
+int* xP=new int[4];
+int* cadenaP=new int[4];
+
+for(xP[0]=0;xP[0]<n;xP[0]++){
+  for(xP[1]=0;xP[1]<n;xP[1]++){
+    for(xP[2]=0;xP[2]<n;xP[2]++){
+      for(xP[3]=0;xP[3]<n;xP[3]++){
+        if((xP[0]!=xP[1]&&xP[0]!=xP[2]&&xP[0]!=xP[3]&&xP[1]!=xP[2]&&xP[1]!=xP[3]&&xP[2]!=xP[3])&&y==0){
+          y=verificar(cadena,xP,x);
+            if(y==1){
+              cadenaP[0]=xP[0];cadenaP[1]=xP[1];cadenaP[2]=xP[2];cadenaP[3]=xP[3];
+              }
+            }
+        }
+     }
+   }
+  }
+return cadenaP;
+}
+
+int main(){
+int n,x;
+cin>>n>>x;
+int* cadena=generar(n);
+int* ar_xP=calcular(cadena,n,x,0);
+ver(cadena,ar_xP);
 return 0;
 }
